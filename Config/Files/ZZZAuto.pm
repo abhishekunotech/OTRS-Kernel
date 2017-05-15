825d89f7290fce33ff65257fe857b1ceb3c25a8b
Abhishek Kulkarni <abhishek.kulkarni@unotechsoft.com>
2017-05-15 20:31:08 +0000

    Added Memcached

--- Config/Files/ZZZAuto.pm
+++ Config/Files/ZZZAuto.pm
@@ -7,14 +7,34 @@ no warnings 'redefine';
 use utf8;
 sub Load {
     my ($File, $Self) = @_;
-$Self->{'SystemID'} = '85';
-$Self->{'FQDN'} = 'b107c238f6e6';
-$Self->{'AdminEmail'} = 'admin@example.com';
-$Self->{'Organization'} = 'Unotech Software';
-$Self->{'LogModule'} = 'Kernel::System::Log::File';
-$Self->{'LogModule::LogFile'} = '/tmp/otrs.log';
-$Self->{'DefaultLanguage'} = 'en';
-$Self->{'CheckMXRecord'} = '0';
-$Self->{'SecureMode'} = 1;
+$Self->{'Cache::Module::Memcached'}->{'Parameters'} =  {
+  'max_size' => '16777216',
+  'utf8' => '1'
+};
+$Self->{'Cache::Module::Memcached'}->{'Servers'} =  [
+  '127.0.0.1:11211'
+];
+$Self->{'Cache::Module'} =  'Kernel::System::Cache::Memcached';
+$Self->{'Customer::AuthModule::LDAP::Params'} =  {
+  'async' => 0,
+  'port' => 389,
+  'timeout' => 120,
+  'version' => 3
+};
+$Self->{'Customer::AuthModule::LDAP::SearchUserPw'} =  '123456';
+$Self->{'Customer::AuthModule::LDAP::SearchUserDN'} =  'uid=UNOTECH225,dc=unotechsoft,dc=com';
+$Self->{'Customer::AuthModule::LDAP::UserAttr'} =  'DN';
+$Self->{'Customer::AuthModule::LDAP::UID'} =  'uid';
+$Self->{'Customer::AuthModule::LDAP::BaseDN'} =  'dc=unotechsoft,dc=com';
+$Self->{'Customer::AuthModule::LDAP::Host'} =  '192.168.2.223';
+$Self->{'Customer::AuthModule'} =  'Kernel::System::CustomerAuth::LDAP';
+delete $Self->{'PreferencesGroups'}->{'SpellDict'};
+$Self->{'LogModule'} =  'Kernel::System::Log::File';
+$Self->{'CheckMXRecord'} =  '0';
+$Self->{'Organization'} =  'Unotech Software';
+delete $Self->{'NodeID'};
+$Self->{'FQDN'} =  'b107c238f6e6';
+$Self->{'SystemID'} =  '85';
+$Self->{'SecureMode'} =  1;
 }
 1;
